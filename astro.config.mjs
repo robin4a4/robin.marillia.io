9373731a47a6144e40665369ec46eb2cd17aaa04
robin marillia <robin4a4@gmail.com>
2023-08-07 22:29:25 +0200

    structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://robin.marillia.io",
   integrations: [mdx(), sitemap()],
-  assetsInclude: ["**/*.glb"],
 });
